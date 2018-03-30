c83916b1635b4bc92fd5196794aed03a6e62d9f7
Jose Contamina <joviman69@gmail.com>
2018-03-30 12:53:15 +0200

    Arreglos visuales en vistas

---
+++ README.md
@@ -0,0 +1,106 @@
+# Nodepop AVANZADO (Práctica del módulo 5)
+NodeJs, Express y MongoDB
+
+### NOTA: Cada vez que se reinicia la App se recarga la base de datos de ejemplo "predefinida"
+
+
+### Features de la API
+
+El esquema del modelo Anuncios es:
+
+    nombre: String  // Contiene el nombre del artículo
+    venta: Boolean  // true indica que el articulo está en venta, mientras que false indica que está en compra
+    precio: Number  // precio del artículo
+    foto: String    // ruta del archivo de imagen del artículo
+    tag: [String]   // etiquetas de categorías del anuncio (work, lifestyle, motor y mobile)
+     
+
+ TODAS las respuestas de la api son a través un JSON compuesto de: 
+~~~
+{
+"success": true, // > Un boleano que nos informa del exito de la consulta
+"result": Resultado // > El resultado obtenido
+}  
+~~~
+Acceso a las características del API
+
+http://servidor:puerto/apiv1/anuncios/
+en esta práctica localhost:3000
+
+Si no especificamos ninguna query a la API, ésta lista todos los anuncios en la base de datos como resultado.
+
+ - Mostrar el total de anuncios en la base de datos como resultado.
+~~~
+http://localhost:3000/apiv1/anuncios/contar
+~~~
+
+ - Mostrar todas las tags de los anuncios como resultado.
+~~~
+http://localhost:3000/apiv1/anuncios/tags
+~~~
+
+- Mostrar el anuncio con una determinada _id.
+~~~
+http://localhost:3000/apiv1/anuncios/<id>
+ejemplo: http://localhost:3000/apiv1/anuncios/5a89c6390774ae0f17e0b61b
+~~~
+
+- Insertar anuncios. (metodo POST)
+~~~
+http://localhost:3000/apiv1/anuncios/
+
+Pasandole el documento
+{nombre: 'Raqueta', venta: true, precio: 300, foto: 'foto10', tag: ['lifestyle']}
+~~~
+
+- Borrar anuncios con una determinada _id. (metodo DELETE)
+~~~
+http://localhost:3000/apiv1/anuncios/<id>
+ejemplo: http://localhost:3000/apiv1/anuncios/5a89c6390774ae0f17e0b61b
+~~~
+
+- Actualizar anuncios con una determinada _id. (metodo PUT)
+~~~
+http://localhost:3000/apiv1/anuncios/<id>
+ejemplo: http://localhost:3000/apiv1/anuncios/5a89c6390774ae0f17e0b61b
+~~~
+
+
+ - Mostrar solo los campos indicados en el resultado, además del _id del documento.
+Admite indicar varios campos separados por espacios
+
+~~~
+http://servidor:puerto/apiv1/anuncios?fields=<campo1 campo2> 
+~~~
+##### Filtros
+
+~~~
+http://servidor:puerto/apiv1/anuncios?<campo>=<valor>
+~~~
+
+    nombre=string                                   // (o subcadena inicial del nombre) 
+    venta=boolean                                   // true = anuncios enta, false = anuncios compra
+    precio=[n] | [min-max] | [min-] | [-max]        // valor exacto o un rango de precios
+    tag=string                                      // filtra por anuncios que contienen dicha tag
+
+**sort**=*campo1 campo2*
+
+admite indicar varios campos separados por espacios
+
+**skip=n**  *ignora los x primeros resultados*
+
+**limit=n**  *limita la salida a n resultados*
+
+### Features especiales
+
+Es posible recargar la coleccion anuncios "predefinida" en la base de datos
+A traves de la siguiente peticion GET a la app:
+~~~
+http://servidor:puerto/anuncios/load
+~~~
+
+Es posible borrar la colección anuncios 
+A traves de la siguiente peticion GET a la app:
+~~~
+http://servidor:puerto/anuncios/clear
+~~~
---
+++ routes/apiv1/anuncios.js
@@ -0,0 +1,175 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const Anuncio = require ('../../models/Anuncio.js');
+
+
+// controlador GET
+// Consultar anuncios
+
+router.get('/', async (req, res, next) => {   
+    try {
+
+        // Extracción a variables de los parámetros de entrada
+        const nombre = req.query.nombre;
+        const venta = req.query.venta;
+        const precio = req.query.precio
+        const tag = req.query.tag;
+        const skip = parseInt(req.query.skip);
+        const limit = parseInt(req.query.limit);
+        const sort = req.query.sort;
+        //const fields = req.query.fields + " -_id"; // Eliminamos el _id de la respuesta
+        const fields = req.query.fields; 
+
+       console.log(req.query);
+          
+        const filtro = {};
+    
+        if (typeof nombre !== 'undefined') { 
+          
+          filtro.nombre = new RegExp('^' + nombre, "i");
+        }
+    
+        if (typeof venta !== 'undefined') {
+          filtro.venta = venta;
+        }
+
+        if (typeof precio !== 'undefined') {
+            if (precio.includes("-")) {
+                const p_min = precio.split("-")[0];
+                const p_max = precio.split("-")[1];
+                console.log("p_min :" + p_min + " p_max: " + p_max );
+                filtro.precio = {$gte: p_min , $lte: p_max };
+
+                if (p_min === "" ) {
+                    console.log("pmin vacio");
+                    filtro.precio = { $lte: p_max };
+                }
+                if (p_max === "" ) {
+                    console.log("pmax vacio");
+                    filtro.precio = { $gte: p_min };
+                }
+            } else { filtro.precio = precio };
+        }
+
+        if (typeof tag !== 'undefined') {
+
+            console.log(tag);
+            const regex = tag.split(" ").join("|");
+            filtro.tag = { $regex: regex, $options: "i" };
+
+          }
+        
+        const docs = await Anuncio.listar(filtro, skip, limit, sort, fields); 
+        
+        res.json({ success: true, result: docs });  
+        //res.json({ docs });  
+      } catch(err) {
+        next(err);
+        return;
+      } 
+});
+
+// controlador GET
+// Contar anuncios 
+
+router.get('/contar', async (req, res, next) => {   
+    try {
+        const total = await Anuncio.find().count().exec();    
+        res.json({ success: true, result: total });  
+
+  } catch(err) {
+        next(err);
+        return;
+  }  
+});
+
+// controlador GET
+// Mostrar tags 
+
+router.get('/tags', async (req, res, next) => {   
+    try {
+        const total = await Anuncio.distinct('tag').exec();    
+        res.json({ success: true, result: total });  
+
+  } catch(err) {
+        next(err);
+        return;
+  }  
+});
+
+
+
+// controlador GET
+// Consultar anuncios por _id
+
+router.get('/:id', async (req, res, next) => {   
+    try {
+        const _id = req.params.id;
+        const docs = await Anuncio.find({_id: _id}).exec();    
+        res.json({ success: true, result: docs });  
+
+  } catch(err) {
+        next(err);
+        return;
+  }  
+});
+
+// controlador POST 
+// Añadir un anuncio
+
+router.post('/', async (req, res, next) => {   
+        try {            
+            const data = req.body;
+            //console.log(req.body);
+            console.log('Nuevo documento creado: ', data);
+            
+            // Creación de nuevo documento basado en el modelo Anuncio para mongoose
+            const anuncio = new Anuncio(data);
+            
+            // Grabación en mongodb por mongoose
+            await anuncio.save((err, anuncioGuardado) => {
+                res.json({ success: true, result: anuncioGuardado });
+                });
+
+        } catch(err) {
+            next(err);
+            return;
+      }  
+    });
+
+
+    // Controlador DELETE /
+    // Eliminación de un anuncio a traves de su _id
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const _id = req.params.id;
+        await Anuncio.remove({_id: _id}).exec();
+        res.json({ success: true });
+    } catch(err) {
+        next(err);
+        return;
+    }
+});
+    
+    // Controlador PUT 
+    // Actualización de un anuncio
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const _id = req.params.id;
+        const data = req.body;
+        const anuncioActualizado = await Anuncio.findByIdAndUpdate(_id, data, { 
+        new: true });    
+        res.json({ success: true, result: anuncioActualizado });
+    
+    } catch(err) {
+        next(err);
+        return;
+  }
+});
+
+module.exports = router;
